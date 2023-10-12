6053a26df3f044922ecf2a4e855705f71144d692
Mehedi Jaman <mail4mjaman@gmail.com>
2023-10-12 23:58:23 +0600

    Fix: - Total Completed Count

--- TodoApp/src/views/TodoView.vue
+++ TodoApp/src/views/TodoView.vue
@@ -99,7 +99,7 @@ const toggleDropdown = (index: number) => {
         <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
           <h2 id="accordion-flush-heading-1">
             <button type="button" class="flex items-center justify-between w-full px-5 py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
-              <span>0 Done</span>
+              <span>{{ completedTodos.length }} Done</span>
               <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
               </svg>
