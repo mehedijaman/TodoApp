198e3b65b80b0778059ac481ae8d9c6e9e1eab8a
RoudroMehedi <roudro.m@gmail.com>
2023-10-12 14:17:31 +0200

    WIP

--- TodoApp/src/stores/todos.ts
+++ TodoApp/src/stores/todos.ts
@@ -1,45 +1,83 @@
 // todos.ts
 
 import { defineStore } from "pinia";
+import { reactive, computed } from "vue";
 
 interface Todo {
   id: number;
   text: string;
   created_at: string;
-  state: string;
 }
 
 export const useTodos = defineStore("todos", {
   state: () => ({
-    todos: [] as Todo[],
-    backlog: [] as Todo[],
-    completedTodos: [] as Todo[], // New array for completed todos
+    todos: reactive([] as Todo[]),
+    backlog: reactive([] as Todo[]),
+    completedTodos: reactive([] as Todo[]),
+    searchTerm: "",
+    sortField: "",
   }),
+  getters: {
+    filteredBacklog(): Todo[] {
+      return this.backlog.filter((todo) =>
+        todo.text.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    },
+    sortedBacklog(): Todo[] {
+      if (this.sortField === "date") {
+        console.log(this.sortField);
+        return this.backlog
+          .slice()
+          .sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+      } else if (this.sortField === "name") {
+        console.log(this.sortField);
+        return this.backlog
+          .slice()
+          .sort((a, b) => a.text.localeCompare(b.text));
+      } else {
+        return this.backlog.slice();
+      }
+    },
+  },
   actions: {
-    addTodo(todo: { text: string; created_at: string; state: string }) {
+    sortBacklog() {
+      if (this.sortField === "date") {
+        this.backlog = this.backlog
+          .slice()
+          .sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+      } else if (this.sortField === "name") {
+        this.backlog = this.backlog
+          .slice()
+          .sort((a, b) => a.text.localeCompare(b.text));
+      }
+    },
+    setSearchTerm(term: string) {
+      this.searchTerm = term;
+    },
+    setSortField(field: string) {
+      this.sortField = field;
+    },
+    addTodo(todo: { text: string; created_at: string }) {
       const randomId = Math.floor(Math.random() * 1000000);
       this.todos.push({ id: randomId, ...todo });
     },
-
     removeTodo(id: number) {
+      console.log(id, "is deleted");
       this.todos = this.todos.filter((todo) => todo.id !== id);
+      console.log(this.todos);
     },
-
     addToBacklog(todo: Todo) {
       this.backlog.push(todo);
       this.removeTodo(todo.id);
+      console.log(this.todos, "current todos");
     },
-
     removeBacklog(id: number) {
       this.backlog = this.backlog.filter((todo) => todo.id !== id);
     },
-
     moveToTodos(todo: Todo) {
       this.todos.push(todo);
       this.removeBacklog(todo.id);
     },
-
-    // Action to mark a todo as completed
     markAsCompleted(todo: Todo) {
       this.completedTodos.push(todo);
       this.removeTodo(todo.id);
